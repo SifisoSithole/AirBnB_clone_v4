616c793a1d279c345a8bdf40514991367a10f27a
nchore27 <nchorefrankley27@gmail.com>
2023-01-29 09:27:15 +0000

    API status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -14,12 +14,12 @@ $(document).ready(() => {
   $.get('http://0.0.0.0:5001/api/v1/status/', (data, status) => {
     if (status === 'success') {
       if (data.status === 'OK') {
-        $('#api_status').addClass('available');
+        $('div#api_status').addClass('available');
       } else {
-        $('#api_status').removeClass('available');
+        $('div#api_status').removeClass('available');
       }
     } else {
-      $('#api_status').removeClass('available');
+      $('div#api_status').removeClass('available');
     }
   });
 });
